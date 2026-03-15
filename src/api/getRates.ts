4930a92d054ec726b442fa4cb6f44d8cc90d0eb2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-15 12:45:17 +0530

    added from and to query parameters to API request URL

--- src/api/getRates.ts
+++ src/api/getRates.ts
@@ -1,3 +1,5 @@
 export async function getRates(amount: number, from: string, to: string) {
   const url = new URL("https://api.frankfurter.app/latest");
   url.searchParams.set("amount", String(amount));
+  url.searchParams.set("from", from);
+  url.searchParams.set("to", to);
