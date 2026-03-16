4f3313c1280eeab02eab50ff05b1ca8f042e97e2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-16 15:53:39 +0530

    Added testing-library imports and CurrencySelect component for tests

--- src/components/CurrencySelect.test.tsx
+++ src/components/CurrencySelect.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { describe, it, expect, vi } from "vitest";
-
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import CurrencySelect from "./CurrencySelect";
 
 
 
