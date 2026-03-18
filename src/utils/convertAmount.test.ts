52c735a22f9041c5923bd848fee30c640a43ed1b
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-18 10:54:38 +0530

    Added test suite for convertAmount function

--- src/utils/convertAmount.test.ts
+++ src/utils/convertAmount.test.ts
@@ -1,7 +1,8 @@
 import { describe, it, expect } from "vitest";
 import { convertAmount } from "./convertAmount";
 
-
+describe("convertAmount", () => {
+  it("computes total and rate correctly", () => {
 
 
 
