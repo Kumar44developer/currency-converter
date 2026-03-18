aab751379ca79501e0c3174be4c8ab10b607590d
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-18 10:47:31 +0530

    Added Result component implementation with formatted output

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -8,7 +8,9 @@ type Props = {
   total: number;
 };
 
-
+export default function Result({ amount, from, to, total }: Props) {
+  const format = new Intl.NumberFormat(undefined, { maximumFractionDigits: 2 });
+  return (
 
 
 
