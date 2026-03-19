5d3f5beb6ad80a4aac8087ed5309fca969660682
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-19 11:48:10 +0530

    Added testing utilities to App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import "@testing-library/jest-dom";
-
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import App from "./App";
 
 
 
