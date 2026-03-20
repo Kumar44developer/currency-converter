b7b1f9f2a10bb7a54a0306cc21c3a16ff1b8b3a2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-20 12:42:55 +0530

    imported SwapButton, Result, and fetchCurrencies modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useMemo, useState } from "react";
 import AmountInput from "./components/AmountInput";
 import CurrencySelect from "./components/CurrencySelect";
-
+import SwapButton from "./components/SwapButton";
+import Result from "./components/Result";
+import { fetchCurrencies } from "./api/fetchCurrencies";
 
 
 
