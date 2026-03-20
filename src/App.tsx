1681a7a52e6a183cdf94e6f45d200dafc8d4f2a0
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-20 12:43:53 +0530

    added state for target currency and conversion result

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,8 @@ import { convertAmount } from "./utils/convertAmount";
 function App() {
   const [amount, setAmount] = useState<string>("1");
   const [from, setFrom] = useState<string>("USD");
-
+  const [to, setTo] = useState<string>("NPR");
+  const [total, setTotal] = useState<number | null>(null);
 
 
 
